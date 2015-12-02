081198ee747d39ac9e44b35cdcf405e808895dba
Michael Lyle <mlyle@lyle.org>
2015-12-01 20:58:03 -0800

    circqueue: add doxygen strings

--- flight/Libraries/circqueue.c
+++ flight/Libraries/circqueue.c
@@ -23,22 +23,25 @@
 #include <circqueue.h>
 
 struct circ_queue {
-	/* Element size in octets */
-	uint16_t elem_size;
-	/* The following are in units of "elements".  The capacity of
-	 * the queue is num_elems - 1 */
-	uint16_t num_elem;
-	volatile uint16_t write_head;
-	volatile uint16_t read_tail;
+	uint16_t elem_size;	/**< Element size in octets */
+	uint16_t num_elem;	/**< Number of elements in circqueue (capacity+1) */
+	volatile uint16_t write_head;	/**< Element position writer is at */
+	volatile uint16_t read_tail;	/**< Element position reader is at */
 
 	/* head == tail: empty.
 	 * head == tail-1: full.
 	 */
 
 	/* This is declared as a uint32_t for alignment reasons. */
-	uint32_t contents[];
+	uint32_t contents[];		/**< Contents of the circula queue */
 };
 
+/** Allocate a new circular queue.
+ * @param[in] elem_size The size of each element, as obtained from sizeof().
+ * @param[in] num_elem The number of elements in the queue.  The capacity is
+ * one less than this (it may not be completely filled.
+ * @returns The handle to the circular queue.
+ */
 circ_queue_t circ_queue_new(uint16_t elem_size, uint16_t num_elem) {
 	PIOS_Assert(elem_size > 0);
 	PIOS_Assert(num_elem > 2);
@@ -56,6 +59,13 @@ circ_queue_t circ_queue_new(uint16_t elem_size, uint16_t num_elem) {
 	return ret;
 }
 
+/** Get a pointer to the current queue write position.
+ * This position is unavailable to any present readers and may be filled in
+ * with the desired data without respect to any synchronization.
+ *
+ * @param[in] q Handle to circular queue.
+ * @returns The position for new data to be written to (of size elem_size).
+ */
 void *circ_queue_cur_write_pos(circ_queue_t q) {
 	void *contents = q->contents;
 
@@ -75,6 +85,15 @@ static inline uint16_t next_pos(uint16_t num_pos, uint16_t current_pos) {
 	return current_pos;
 }
 
+/** Makes the current block of data available to readers and advances write pos.
+ * This may fail if the queue contain num_elems -1 elements, in which case the
+ * advance may be retried in the future.  In this case, data already written to
+ * write_pos is preserved and the advance may be retried (or overwritten with
+ * new data).
+ *
+ * @param[in] q Handle to circular queue.
+ * @returns 0 if the write succeeded, nonzero on error.
+ */
 int circ_queue_advance_write(circ_queue_t q) {
 	uint16_t new_write_head = next_pos(q->num_elem, q->write_head);
 
@@ -90,6 +109,14 @@ int circ_queue_advance_write(circ_queue_t q) {
 	return 0;
 }
 
+/** Returns a block of data to the reader.
+ * The block is "claimed" until released with circ_queue_read_completed.
+ * No new data is available until that call is made (instead the same
+ * block-in-progress will be returned).
+ *
+ * @param[in] q Handle to circular queue.
+ * @returns pointer to the data, or NULL if the queue is empty.
+ */
 void *circ_queue_read_pos(circ_queue_t q) {
 	uint16_t read_tail = q->read_tail;
 	void *contents = q->contents;
@@ -102,6 +129,12 @@ void *circ_queue_read_pos(circ_queue_t q) {
 	return contents + q->read_tail * q->elem_size;
 }
 
+/** Releases a block of read data obtained by circ_queue_read_pos.
+ * Behavior is undefined if circ_queue_read_pos did not previously return
+ * a block of data.
+ *
+ * @param[in] q Handle to the circular queue.
+ */
 void circ_queue_read_completed(circ_queue_t q) {
 	/* Avoid multiple accesses to a volatile */
 	uint16_t read_tail = q->read_tail;
