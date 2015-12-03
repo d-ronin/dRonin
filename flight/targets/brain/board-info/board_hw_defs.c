fe67c15e97ac8299dd2dd544a340f0f99557235d
Michael Lyle <mlyle@lyle.org>
2015-12-02 16:06:24 -0800

    brain: use new ADC

--- flight/targets/brain/board-info/board_hw_defs.c
+++ flight/targets/brain/board-info/board_hw_defs.c
@@ -1567,6 +1567,14 @@ struct pios_internal_adc_cfg pios_adc_cfg = {
     },
     .half_flag = DMA_IT_HTIF4,
     .full_flag = DMA_IT_TCIF4,
+    .adc_pins = {
+	    { GPIOC, GPIO_Pin_0, ADC_Channel_10 },
+	    { GPIOC, GPIO_Pin_1, ADC_Channel_11 },
+	    { GPIOC, GPIO_Pin_3, ADC_Channel_13 },
+	    { NULL, 0, ADC_Channel_Vrefint },	/* Voltage reference */
+	    { NULL, 0, ADC_Channel_TempSensor }	/* Temperature sensor */
+    },
+    .adc_pin_count = 5
 };
 
 void PIOS_ADC_DMA_irq_handler(void)
