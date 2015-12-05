8b0b772eeaba98bcc7e42a7c48b1ad27b3e36701
Michael Lyle <mlyle@lyle.org>
2015-12-04 23:59:29 -0800

    flight: f1: use expf/powf approximations to hopefully drop fewer points
    issue #112

--- flight/Libraries/math/misc_math.h
+++ flight/Libraries/math/misc_math.h
@@ -78,6 +78,94 @@ static inline bool IS_NOT_FINITE(float x) {
 	return (!isfinite(x));
 }
 
+/* Following functions from fastapprox https://code.google.com/p/fastapprox/
+ * are governed by this license agreement: */
+
+/*=====================================================================*
+ *                   Copyright (C) 2011 Paul Mineiro                   *
+ * All rights reserved.                                                *
+ *                                                                     *
+ * Redistribution and use in source and binary forms, with             *
+ * or without modification, are permitted provided that the            *
+ * following conditions are met:                                       *
+ *                                                                     *
+ *     * Redistributions of source code must retain the                *
+ *     above copyright notice, this list of conditions and             *
+ *     the following disclaimer.                                       *
+ *                                                                     *
+ *     * Redistributions in binary form must reproduce the             *
+ *     above copyright notice, this list of conditions and             *
+ *     the following disclaimer in the documentation and/or            *
+ *     other materials provided with the distribution.                 *
+ *                                                                     *
+ *     * Neither the name of Paul Mineiro nor the names                *
+ *     of other contributors may be used to endorse or promote         *
+ *     products derived from this software without specific            *
+ *     prior written permission.                                       *
+ *                                                                     *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND              *
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,         *
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES               *
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE             *
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER               *
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                 *
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES            *
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE           *
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR                *
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF          *
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT           *
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY              *
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE             *
+ * POSSIBILITY OF SUCH DAMAGE.                                         *
+ *                                                                     *
+ * Contact: Paul Mineiro <paul@mineiro.com>                            *
+ *=====================================================================*/
+
+static inline float 
+fastlog2 (float x)
+{
+	union { float f; uint32_t i; } vx = { x };
+	union { uint32_t i; float f; } mx = { (vx.i & 0x007FFFFF) | (0x7e << 23) };
+	float y = vx.i;
+	y *= 1.0f / (1 << 23);
+
+	return y - 124.22551499f
+		- 1.498030302f * mx.f 
+		- 1.72587999f / (0.3520887068f + mx.f);
+}
+
+static inline float
+fastpow2 (float p)
+{
+	float offset = (p < 0) ? 1.0f : 0.0f;
+	int w = p;
+	float z = p - w + offset;
+	union { uint32_t i; float f; } v = { (1 << 23) * (p + 121.2740838f + 27.7280233f / (4.84252568f - z) - 1.49012907f * z) };
+
+	return v.f;
+}
+
+
+static inline float
+fastpow (float x, float p)
+{
+	return fastpow2 (p * fastlog2 (x));
+}
+
+static inline float
+fastexp (float p)
+{
+	  return fastpow2 (1.442695040f * p);
+}
+
+#ifdef SMALLF1
+#define powapprox fastpow
+#define expapprox fastexp
+#else
+#define powapprox powf
+#define expapprox expf
+#endif
+
 #endif /* MISC_MATH_H */
 
 /**
