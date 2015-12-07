1bb9dfce650c1df857ce2c81d1671e40c4abdc37
Martin Luessi <mluessi@gmail.com>
2015-12-07 14:41:12 -0500

    UAVO Man: fix unused list usage

--- flight/UAVObjects/uavobjectmanager.c
+++ flight/UAVObjects/uavobjectmanager.c
@@ -8,6 +8,7 @@
  * @file       uavobjectmanager.c
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
  * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2014
+ * @author     dRonin, http://dronin.org Copyright (C) 2015
  * @brief      Object manager library. This library holds a collection of all objects.
  *             It can be used by all modules/libraries to find an object reference.
  * @see        The GNU Public License (GPL) Version 3
@@ -2007,18 +2008,18 @@ static int32_t connectObj(UAVObjHandle obj_handle, struct pios_queue *queue,
 	}
 
 	int mallocSize = sizeof(*event);
-	struct ObjectEventEntry * unused = events_unused;
+	struct ObjectEventEntry ** unused = &events_unused;
 
 	if (interval) {
 		mallocSize = sizeof(*throttled);
-		unused = events_unused_throttled;
+		unused = &events_unused_throttled;
 	}
 
 	PIOS_Recursive_Mutex_Lock(mutex, PIOS_MUTEX_TIMEOUT_MAX);
-	if (unused != NULL) {
+	if (*unused != NULL) {
 		// We can re-use the memory of a previously disconnected event
-		event = unused;
-		LL_DELETE(unused, event);
+		event = *unused;
+		LL_DELETE(*unused, event);
 	}
 	else {
 		event =	(struct ObjectEventEntry *) PIOS_malloc_no_dma(mallocSize);
@@ -2029,6 +2030,7 @@ static int32_t connectObj(UAVObjHandle obj_handle, struct pios_queue *queue,
 	}
 	PIOS_Recursive_Mutex_Unlock(mutex);
 
+	memset(event, 0, mallocSize);
 	event->cb = cb;
 
 	if (!cb) {
