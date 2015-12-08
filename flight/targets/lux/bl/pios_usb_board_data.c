2ca96f25975742cfb767eb75a10a4b1dce8f1b88
Michael Lyle <mlyle@lyle.org>
2015-12-08 12:03:57 -0800

    lux: cleanup.  Fix stack checking, product name

--- flight/targets/lux/bl/pios_usb_board_data.c
+++ flight/targets/lux/bl/pios_usb_board_data.c
@@ -34,11 +34,9 @@
 static const uint8_t usb_product_id[14] = {
 	sizeof(usb_product_id),
 	USB_DESC_TYPE_STRING,
-	'A', 0,
-	'c', 0,
-	'B', 0,
-	'r', 0,
-	'o', 0,
+	'L', 0,
+	'u', 0,
+	'x', 0,
 };
 
 static uint8_t usb_serial_number[2 + PIOS_SYS_SERIAL_NUM_ASCII_LEN*2 + (sizeof(PIOS_USB_BOARD_SN_SUFFIX)-1)*2] = {
@@ -55,14 +53,14 @@ static const struct usb_string_langid usb_lang_id = {
 static const uint8_t usb_vendor_id[18] = {
 	sizeof(usb_vendor_id),
 	USB_DESC_TYPE_STRING,
-	'T', 0,
-	'a', 0,
-	'u', 0,
-	' ', 0,
 	'L', 0,
-	'a', 0,
-	'b', 0,
-	's', 0,
+	'u', 0,
+	'm', 0,
+	'e', 0,
+	'n', 0,
+	'i', 0,
+	'e', 0,
+	'r', 0,
 };
 
 int32_t PIOS_USB_BOARD_DATA_Init(void)
