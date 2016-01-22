52a0f5e161b3efc8f29853d5cb69e6d1364e6059
Michael Corcoran <mrc93@uclive.ac.nz>
2016-01-23 09:08:23 +1300

    UAVOGen:GCS: Work around QT37241 by not exporting int8 to QML

--- ground/uavobjgenerator/generators/gcs/uavobjectgeneratorgcs.h
+++ ground/uavobjgenerator/generators/gcs/uavobjectgeneratorgcs.h
@@ -44,7 +44,7 @@ private:
     QString escape_raw_string(QString raw);
 
     QString gcsCodeTemplate,gcsIncludeTemplate;
-    QStringList fieldTypeStrCPP,fieldTypeStrCPPClass;
+    QStringList fieldTypeStrCPP, fieldTypeStrQML,fieldTypeStrCPPClass;
     QDir gcsCodePath;
     QDir gcsOutputPath;
 };
