51c83e927338ef80018e614d36d760a54af05ed0
Michael Corcoran <mrc93@uclive.ac.nz>
2016-01-23 20:39:17 +1300

    FlyingF3: Remove FreeRTOS

--- flight/targets/flyingf3/fw/pios_config.h
+++ flight/targets/flyingf3/fw/pios_config.h
@@ -120,7 +120,7 @@
  *
  * IDLE_COUNTS_PER_SEC_AT_NO_LOAD = (uint32_t)((double)idleCounter / xTickCount * 1000 + 0.5)
  *
- * This has to be redone every time the toolchain, toolchain flags or FreeRTOS
+ * This has to be redone every time the toolchain, toolchain flags or RTOS
  * configuration like number of task priorities or similar changes.
  * A change in the cpu load calculation or the idle task handler will invalidate this as well.
  */
