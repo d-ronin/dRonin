3299567d333399b9fb950b683792462fb76645a6
Michael Lyle <mlyle@lyle.org>
2016-01-28 18:53:22 -0800

    gcs: take less of forever to connect.
    Also update the connection status object more frequently so we're less
    likely to spuriously lose connection from little blips / lost messages.

--- ground/gcs/src/plugins/uploader/tl_dfu.cpp
+++ ground/gcs/src/plugins/uploader/tl_dfu.cpp
@@ -2,6 +2,7 @@
  ******************************************************************************
  *
  * @file       tl_dfu.cpp
+ * @author     dRonin, http://dRonin.org/, Copyright (C) 2016
  * @author     Tau Labs, http://taulabs.org, Copyright (C) 2014
  * @addtogroup GCSPlugins GCS Plugins
  * @{
@@ -23,7 +24,12 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Additional note on redistribution: The copyright and license notices above
+ * must be maintained in each individual source file that is a derivative work
+ * of this source file; otherwise redistribution is prohibited.
  */
+
 
 #include "tl_dfu.h"
 
