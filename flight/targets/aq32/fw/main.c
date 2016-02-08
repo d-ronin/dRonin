98cb02e2821312363a129362e263fb0a6289cef1
Michael Corcoran <mrc93@uclive.ac.nz>
2016-02-08 16:33:25 +1300

    AQ32: Remove FreeRTOS

--- flight/targets/aq32/fw/main.c
+++ flight/targets/aq32/fw/main.c
@@ -7,7 +7,7 @@
  *
  * @file       main.c 
  * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2014
- * @brief      Start FreeRTOS and the Modules.
+ * @brief      Start RTOS and the Modules.
  * @see        The GNU Public License (GPL) Version 3
  * 
  *****************************************************************************/
@@ -34,11 +34,6 @@
 #include "systemmod.h"
 #include "pios_thread.h"
 
-#if defined(PIOS_INCLUDE_FREERTOS)
-#include "FreeRTOS.h"
-#include "task.h"
-#endif /* defined(PIOS_INCLUDE_FREERTOS) */
-
 /* Prototype of PIOS_Board_Init() function */
 extern void PIOS_Board_Init(void);
 extern void Stack_Change(void);
@@ -56,7 +51,7 @@ static void initTask(void *parameters);
 *
 * Initialize PiOS<BR>
 * Create the "System" task (SystemModInitializein Modules/System/systemmod.c) <BR>
-* Start FreeRTOS Scheduler (vTaskStartScheduler)<BR>
+* Start the RTOS Scheduler<BR>
 * If something goes wrong, blink LED1 and LED2 every 100ms
 *
 */
@@ -76,25 +71,13 @@ int main()
 	/* Brings up System using CMSIS functions, enables the LEDs. */
 	PIOS_SYS_Init();
 
-	/* For Revolution we use a FreeRTOS task to bring up the system so we can */
-	/* always rely on FreeRTOS primitive */
+	/* For Revolution we use an RTOS task to bring up the system so we can */
+	/* always rely on an RTOS primitive */
 	initTaskHandle = PIOS_Thread_Create(initTask, "init", INIT_TASK_STACK, NULL, INIT_TASK_PRIORITY);
 	PIOS_Assert(initTaskHandle != NULL);
 
-#if defined(PIOS_INCLUDE_FREERTOS)
-	/* Start the FreeRTOS scheduler */
-	vTaskStartScheduler();
-
-	/* If all is well we will never reach here as the scheduler will now be running. */
-	/* Do some PIOS_LED_HEARTBEAT to user that something bad just happened */
-	PIOS_LED_Off(PIOS_LED_HEARTBEAT); \
-	for(;;) { \
-		PIOS_LED_Toggle(PIOS_LED_HEARTBEAT); \
-		PIOS_DELAY_WaitmS(100); \
-	};
-#elif defined(PIOS_INCLUDE_CHIBIOS)
 	PIOS_Thread_Sleep(PIOS_THREAD_TIMEOUT_MAX);
-#endif /* defined(PIOS_INCLUDE_CHIBIOS) */
+
 	return 0;
 }
 /**
