d8e894f6a8ccddef366fb0be1c4825a1c8dba03f
droney <fredhartnett@outlook.com>
2016-02-08 19:59:47 -0600

    AH task throttle_desired integral caluclation err
    The current altitude hold loop has two active paths which allow control to
    be passed to the throttle_desired pid_apply_antiwindup() function during
    flight. The first is the loop timeout specified by dt_ms and the second is
    changes to the AltitudeHoldDesired object. The latter occurs every 20 ms
    as defined by the ManualControl task rate. Unfortunately, when the
    AltitudeHoldeDesired object is updated, regardles of whether the throttle
    changed or not, the pid_apply_antiwindup is called at some time interval
    less than dt_ms. Therfore a velocity error is computed for this shortened
    time interval yet pid_apply_antiwindup is still passed the dt_ms time
    value, resulting in a desired_velocity PID integral calculation error.
    
    I am opening this up for the experts to weigh in on this proposed change
    where the ManualControl and AltitudeHold task be performed independent of
    each other, where altitudeHoldTask will sample AltitudeHoldDesired only
    when the dt_ms timer expires. This proposed change eliminates the integral
    calculation error associated with the incorrect velocity error calculated
    off cycle from dt_ms. I further suggest this change allows the AH task to
    run at a more reasonable loop rate (20ms or longer), thereby freeing up
    cpu resources.

--- flight/Modules/AltitudeHold/altitudehold.c
+++ flight/Modules/AltitudeHold/altitudehold.c
@@ -173,6 +173,7 @@ static void altitudeHoldTask(void *parameters)
 			timeout = engaged ? dt_ms : 100;
 
 			continue;
+
 		} else if (ev.obj == AltitudeHoldSettingsHandle()) {
 			AltitudeHoldSettingsGet(&altitudeHoldSettings);
 
