df91ebb85975c238198fe1005fc2cb38583ac208
droney <fredhartnett@outlook.com>
2016-02-08 18:43:33 -0600

    The current altitude hold loop has two active paths which allow control to be passed to the throttle_desired pid_apply_antiwindup() function during flight. The first is the loop timeout specified by dt_ms and the second is changes to the AltitudeHoldDesired object. The latter occurs every 20 ms as defined by the ManualControl task rate. Unfortunately, when the AltitudeHoldeDesired object is updated, regardles of whether the throttle changed or not, the pid_apply_antiwindup is called at some time interval less than dt_ms. Therfore a velocity error is computed for this shortened time interval yet pid_apply_antiwindup is still passed the dt_ms time value, resulting in a desired_velocity PID integral calculation error.
    I am opening this up for the experts to weigh in on this proposed change
    where the ManualControl and AltitudeHold task be performed independent of
    each other, where altitudeHoldTask will sample AltitudeHoldDesired only when
    the dt_ms timer expires. This proposed change eliminates the integral
    calculation error associated with the incorrect velocity error calculated
    off cycle from dt_ms. I further suggest this change allows the AH task to run
    at a more reasonable loop rate (20ms or longer), thereby freeing up cpu
    resources.

--- flight/Modules/AltitudeHold/altitudehold.c
+++ flight/Modules/AltitudeHold/altitudehold.c
@@ -134,7 +134,6 @@ static void altitudeHoldTask(void *parameters)
 	struct pid velocity_pid;
 
 	// Listen for object updates.
-	AltitudeHoldDesiredConnectQueue(queue);
 	AltitudeHoldSettingsConnectQueue(queue);
 	FlightStatusConnectQueue(queue);
 
@@ -145,7 +144,7 @@ static void altitudeHoldTask(void *parameters)
 	AlarmsSet(SYSTEMALARMS_ALARM_ALTITUDEHOLD, SYSTEMALARMS_ALARM_OK);
 
 	// Main task loop
-	const uint32_t dt_ms = 5;
+	const uint32_t dt_ms = 20;
 	const float dt_s = dt_ms * 0.001f;
 	uint32_t timeout = dt_ms;
 
@@ -162,25 +161,19 @@ static void altitudeHoldTask(void *parameters)
 				StabilizationDesiredThrottleGet(&velocity_pid.iAccumulator);
 				engaged = true;
 
-				// Make sure this uses a valid AltitudeHoldDesired. No delay is really required here
-				// because ManualControl sets AltitudeHoldDesired first before the FlightStatus, but
-				// this is just to be conservative at 1ms when engaging will not bother the pilot.
-				PIOS_Thread_Sleep(1);
-				AltitudeHoldDesiredGet(&altitudeHoldDesired);
-
 			} else if (flight_mode != FLIGHTSTATUS_FLIGHTMODE_ALTITUDEHOLD)
 				engaged = false;
 
 			// Run loop at 20 Hz when engaged otherwise just slowly wait for it to be engaged
 			timeout = engaged ? dt_ms : 100;
 
-		} else if (ev.obj == AltitudeHoldDesiredHandle()) {
-			AltitudeHoldDesiredGet(&altitudeHoldDesired);
+			continue;
 		} else if (ev.obj == AltitudeHoldSettingsHandle()) {
 			AltitudeHoldSettingsGet(&altitudeHoldSettings);
 
 			pid_configure(&velocity_pid, altitudeHoldSettings.VelocityKp,
 				          altitudeHoldSettings.VelocityKi, 0.0f, 1.0f);
+			continue;
 		}
 
 		bool landing = altitudeHoldDesired.Land == ALTITUDEHOLDDESIRED_LAND_TRUE;
@@ -199,6 +192,7 @@ static void altitudeHoldTask(void *parameters)
 			velocity_z = -velocity_z; // Use positive up convention
 
 			// Compute the altitude error
+			AltitudeHoldDesiredGet(&altitudeHoldDesired);
 			altitude_error = altitudeHoldDesired.Altitude - position_z;
 
 			// Velocity desired is from the outer controller plus the set point
