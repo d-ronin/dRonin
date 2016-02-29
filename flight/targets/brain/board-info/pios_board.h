e9ef7ff8741a21b8002651e770b287e34eb2deea
jihlein <jihlein@hotmail.com>
2016-02-29 16:28:35 -0700

    Flight: Storm32bgc integration with remaining F4 targets

--- flight/targets/brain/board-info/pios_board.h
+++ flight/targets/brain/board-info/pios_board.h
@@ -129,7 +129,7 @@ extern uintptr_t pios_com_hott_id;
 extern uintptr_t pios_com_picoc_id;
 extern uintptr_t pios_com_logging_id;
 extern uintptr_t pios_com_openlog_logging_id;
-
+extern uintptr_t pios_com_storm32bgc_id;
 
 #define PIOS_COM_GPS                    (pios_com_gps_id)
 #define PIOS_COM_TELEM_USB              (pios_com_telem_usb_id)
@@ -144,6 +144,7 @@ extern uintptr_t pios_com_openlog_logging_id;
 #define PIOS_COM_PICOC                  (pios_com_picoc_id)
 #define PIOS_COM_SPIFLASH               (pios_com_logging_id)
 #define PIOS_COM_OPENLOG                (pios_com_openlog_logging_id)
+#define PIOS_COM_STORM32BGC				(pios_com_storm32bgc_id)
 
 #if defined(PIOS_INCLUDE_DEBUG_CONSOLE)
 extern uintptr_t pios_com_debug_id;
