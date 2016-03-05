d4bd647528a5cbe6074c9edba6ca96c160939c3a
Michael Lyle <mlyle@lyle.org>
2016-03-05 10:18:09 -0800

    flight: create stub STM32F10x library file, work on upgrader

---
+++ flight/PiOS/STM32F10x/library.mk
@@ -0,0 +1,37 @@
+
+#
+# Compiler options implied by the F4xx
+#
+CDEFS				+= -DSTM32F10X
+CDEFS				+= -DSTM32F10X_MD
+CDEFS				+= -DHSE_VALUE=$(OSCILLATOR_FREQ)
+CDEFS 				+= -DUSE_STDPERIPH_DRIVER
+ARCHFLAGS			+= -mcpu=cortex-m3
+FLOATABI			+= soft
+
+#
+# PIOS device library source and includes
+#
+EXTRAINCDIRS		+=	$(PIOS_DEVLIB)/inc
+
+#
+# CMSIS for the F4
+#
+include $(PIOSCOMMONLIB)/CMSIS3/library.mk
+CMSIS_DEVICEDIR	:=	$(PIOS_DEVLIB)/Libraries/CMSIS/Core/CM3
+SRC			+=	$(BOARD_INFO_DIR)/cmsis_system.c
+EXTRAINCDIRS		+=	$(CMSIS_DEVICEDIR)
+
+#
+# ST Peripheral library
+#
+PERIPHLIB			 =	$(PIOS_DEVLIB)/Libraries/STM32F10x_StdPeriph_Driver
+EXTRAINCDIRS		+=	$(PERIPHLIB)/inc
+SRC					+=	$(wildcard $(PERIPHLIB)/src/*.c)
+
+#
+# ST USB Device library
+#
+USBDEVLIB			=	$(PIOS_DEVLIB)/Libraries/STM32_USB_Device_Library
+EXTRAINCDIRS			+=	$(USBDEVLIB)/Core/inc
+SRC				+=	$(wildcard $(USBDEVLIB)/Core/src/*.c)
