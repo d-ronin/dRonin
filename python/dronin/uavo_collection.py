f92ab71596748b6dc0ae4c4da81b468a90aff1e6
Michael Lyle <mlyle@lyle.org>
2016-03-08 14:34:36 -0800

    python: uavo_collection: teach to accept a tarfile obj

--- python/dronin/uavo_collection.py
+++ python/dronin/uavo_collection.py
@@ -32,26 +32,7 @@ class UAVOCollection(dict):
 
         return objs
 
-    def from_git_hash(self, githash):
-        import subprocess
-        import tarfile
-        from cStringIO import StringIO
-        # Get the directory where the code is located
-        tl_dir = op.join(op.dirname(__file__), "..", "..")
-        #
-        # Grab the exact uavo definition files from the git repo using the header's git hash
-        #
-        p = subprocess.Popen(['git', 'archive', githash, '--', 'shared/uavobjectdefinition/'],
-                             stdout=subprocess.PIPE, cwd=tl_dir)
-        # grab the tar file data
-        git_archive_data, git_archive_errors = p.communicate()
-
-        # coerce the tar file data into a file object so that tarfile likes it
-        fobj = StringIO(git_archive_data)
-
-        # feed the tar file data to a tarfile object
-        t = tarfile.open(fileobj=fobj)
-
+    def from_tar_file(self, t):
         # Get the file members
         f_members = []
         for f_info in t.getmembers():
@@ -71,6 +52,28 @@ class UAVOCollection(dict):
 
             # add this uavo definition to our dictionary
             self.update([('{0:08x}'.format(u._id), u)])
+
+    def from_git_hash(self, githash):
+        import subprocess
+        import tarfile
+        from cStringIO import StringIO
+        # Get the directory where the code is located
+        src_dir = op.join(op.dirname(__file__), "..", "..")
+        #
+        # Grab the exact uavo definition files from the git repo using the header's git hash
+        #
+        p = subprocess.Popen(['git', 'archive', githash, '--', 'shared/uavobjectdefinition/'],
+                             stdout=subprocess.PIPE, cwd=src_dir)
+        # grab the tar file data
+        git_archive_data, git_archive_errors = p.communicate()
+
+        # coerce the tar file data into a file object so that tarfile likes it
+        fobj = StringIO(git_archive_data)
+
+        # feed the tar file data to a tarfile object
+        t = tarfile.open(fileobj=fobj)
+
+        self.from_tar_file(t)
 
     def from_uavo_xml_path(self, path):
         import os
