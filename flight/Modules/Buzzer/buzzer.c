f67c6cf823b174cb038c5be0cfb5cf9b1d9c0ca6
henn1001 <hendrik-schlee@web.de>
2016-03-14 23:03:10 +0100

    Buzzermodule - optimizations

--- flight/Modules/Buzzer/buzzer.c
+++ flight/Modules/Buzzer/buzzer.c
@@ -1,18 +1,12 @@
 /**
  ******************************************************************************
- * @addtogroup TauLabsModules Tau Labs Modules
- * @{
- * @addtogroup BuzzerModule Buzzer Module
- * @{
- *
  * @file       buzzer.c
- * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
- * @author     Tau Labs, http://taulabs.org, Copyright (C) 2013-2014
- * @author     dRonin, http://dronin.org Copyright (C) 2016
- * @brief      Module to read alarms and turns on/off a buzzer.
- *
- * @see        The GNU Public License (GPL) Version 3
- *
+ * @author     dRonin, http://dRonin.org/, Copyright (C) 2016
+ * @addtogroup dRoninModules dRonin Modules
+ * @{
+ * @addtogroup Buzzer Buzzer Module
+ * @{
+ * @brief      Generates visible and audible alarms
  *****************************************************************************/
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -44,7 +38,7 @@
 
 // ****************
 // Private constants
-#define STACK_SIZE_BYTES            420  //gcs shows ~40 stack remaining
+#define STACK_SIZE_BYTES            430
 #define TASK_PRIORITY               PIOS_THREAD_PRIO_LOW
 #define SAMPLE_PERIOD_MS            200
 // Private types
@@ -52,22 +46,17 @@
 // Private variables
 static bool module_enabled = false;
 static struct pios_thread *buzzerTaskHandle;
-static int8_t buzzerPin = -1; //Pin for buzzer
-static int8_t accChan = -1; //Accessory Channel
 static bool buzzer_settings_updated;
-static bool wasArmed;
-static bool wasDisarmed;
-static 	FlightBuzzerSettingsData buzzerSettings;
 
 // ****************
 // Private functions
 static void buzzerTask(void * parameters);
-static void getTune(uint8_t arming, uint32_t *newTune);
+static uint32_t getTune(uint8_t arming, bool was_armed, bool was_disarmed, FlightBuzzerSettingsData buzzerSettings);
 
 static int32_t BuzzerStart(void)
 {
 	if (module_enabled) {
-		//FlightBuzzerSettingsConnectCallbackCtx(UAVObjCbSetFlag, &buzzer_settings_updated);
+		FlightBuzzerSettingsConnectCallbackCtx(UAVObjCbSetFlag, &buzzer_settings_updated);
 
 		// Start tasks
 		buzzerTaskHandle = PIOS_Thread_Create(buzzerTask, "buzzerBridge", STACK_SIZE_BYTES, NULL, TASK_PRIORITY);
@@ -105,10 +94,11 @@ MODULE_INITCALL(BuzzerInitialize, BuzzerStart);
  */
 static void buzzerTask(void * parameters)
 {
+	FlightBuzzerSettingsData buzzerSettings;
 	
 	buzzer_settings_updated = true;
-	wasArmed = false;
-	wasDisarmed = true;
+	bool was_armed = false;
+	bool was_disarmed = true;
 
 	// Main task loop
 	uint32_t lastSysTime;
@@ -120,30 +110,28 @@ static void buzzerTask(void * parameters)
 		if (buzzer_settings_updated) {
 			buzzer_settings_updated = false;
 			FlightBuzzerSettingsGet(&buzzerSettings);
-
-			accChan = buzzerSettings.AcessoryChan;
-			buzzerPin = buzzerSettings.BuzzerPin;
-			if (buzzerPin == 0)
-				buzzerPin = -1;
 		}
 
 
 		uint8_t arming;
 		FlightStatusArmedGet(&arming);
 		
-		if ((arming == FLIGHTSTATUS_ARMED_ARMED) && wasDisarmed)
-			wasArmed = true;
+		if ((arming == FLIGHTSTATUS_ARMED_ARMED) && was_disarmed)
+			was_armed = true;
 
-		if ((arming == FLIGHTSTATUS_ARMED_DISARMED) && wasArmed)
-			wasDisarmed = true;
+		if ((arming == FLIGHTSTATUS_ARMED_DISARMED) && was_armed)
+			was_disarmed = true;
 
 
 		static uint32_t currTune = 0;
 		static uint32_t currTuneState = 0;
-		uint32_t newTune = 0;
 
 		//get a new tune
-		getTune(arming, &newTune);
+		uint32_t newTune = getTune(arming, was_armed, was_disarmed, buzzerSettings);
+		
+		//make sure arming is only triggered once
+		if ((arming == FLIGHTSTATUS_ARMED_ARMED) && was_disarmed)
+			was_disarmed = false;
 
 		// Do we need to change tune?
 		if ((newTune != 0) && (currTuneState == 0)) {
@@ -163,52 +151,51 @@ static void buzzerTask(void * parameters)
 		}
 		
 		if (buzzOn)
-			PIOS_GPIO_Off(buzzerPin);  //needs a inversion check for npn/pnp transistor
+			PIOS_GPIO_Off(buzzerSettings.BuzzerPin);  //needs a inversion check for npn/pnp transistor
 		else
-			PIOS_GPIO_On(buzzerPin);
+			PIOS_GPIO_On(buzzerSettings.BuzzerPin);
 
 	}
 }
 
 
-static void getTune(uint8_t arming, uint32_t *newTune)
+static uint32_t getTune(uint8_t arming, bool was_armed, bool was_disarmed, FlightBuzzerSettingsData buzzerSettings)
 {
 	//Failsafe
-	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_FAILSAFE] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && wasArmed)
+	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_FAILSAFE] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && was_armed)
 			|| (buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_FAILSAFE] == FLIGHTBUZZERSETTINGS_ALARMS_ON)) {
 		
 		uint8_t mode;
 		FlightStatusFlightModeGet(&mode);
 		
 		if(mode == FLIGHTSTATUS_FLIGHTMODE_FAILSAFE) {
-			*newTune=0b1100000;
-			return;
+			uint32_t newTune=0b1100000;
+			return newTune;
 			}
 	}
 	//Accessory
-	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_ACCESSORY] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && wasArmed)
+	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_ACCESSORY] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && was_armed)
 			|| (buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_ACCESSORY] == FLIGHTBUZZERSETTINGS_ALARMS_ON)) {
 		
 		AccessoryDesiredData acc;
-		AccessoryDesiredInstGet(accChan, &acc);
+		AccessoryDesiredInstGet(buzzerSettings.AcessoryChan, &acc);
 		
 		if(acc.AccessoryVal > 0.5f) {
-			*newTune=0b1100000;
-			return;
+			uint32_t newTune=0b1100000;
+			return newTune;
 		}
 	}
 	//Arming
-	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_ARMING] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && wasArmed)
+	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_ARMING] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && was_armed)
 			|| (buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_ARMING] == FLIGHTBUZZERSETTINGS_ALARMS_ON)) {
 		
-		if((arming == FLIGHTSTATUS_ARMED_ARMED) && wasDisarmed) {
-			*newTune=0b1010;
-			wasDisarmed = false;
-			return;
+		if((arming == FLIGHTSTATUS_ARMED_ARMED) && was_disarmed) {
+			uint32_t newTune=0b1010;
+			return newTune;
 		}
 	}
 	//Flightmode
-	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_FLIGHTMODE] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && wasArmed)
+	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_FLIGHTMODE] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && was_armed)
 			|| (buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_FLIGHTMODE] == FLIGHTBUZZERSETTINGS_ALARMS_ON)) {
 		
 		uint8_t mode;
@@ -216,42 +203,40 @@ static void getTune(uint8_t arming, uint32_t *newTune)
 		FlightStatusFlightModeGet(&mode);
 		
 		if((mode != lastMode) && (mode != FLIGHTSTATUS_FLIGHTMODE_FAILSAFE)) {
-			*newTune=0b10;
+			uint32_t newTune=0b10;
 			lastMode = mode;
-			return;
+			return newTune;
 		}
 	}
 	//Battery
-	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_BATWAR] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && wasArmed)
+	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_BATWAR] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && was_armed)
 			|| (buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_BATWAR] == FLIGHTBUZZERSETTINGS_ALARMS_ON)) {
 		
 		if(AlarmsGet(SYSTEMALARMS_ALARM_BATTERY) == SYSTEMALARMS_ALARM_WARNING) {
-			*newTune=0b100100100;
-			return;
+			uint32_t newTune=0b100100100;
+			return newTune;
 		}
 		else if (AlarmsGet(SYSTEMALARMS_ALARM_BATTERY) > SYSTEMALARMS_ALARM_WARNING) {
-			*newTune=0b100100100;
-			return;
+			uint32_t newTune=0b100100100;
+			return newTune;
 		}
 	}
 	//Gps
-	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_GPSWAR] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && wasArmed)
+	if (((buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_GPSWAR] == FLIGHTBUZZERSETTINGS_ALARMS_WASARMED) && was_armed)
 			|| (buzzerSettings.Alarms[FLIGHTBUZZERSETTINGS_ALARMS_GPSWAR] == FLIGHTBUZZERSETTINGS_ALARMS_ON)) {
 		
 		if (AlarmsGet(SYSTEMALARMS_ALARM_GPS) == SYSTEMALARMS_ALARM_WARNING) {
-			*newTune=0b100100100;
-			return;
+			uint32_t newTune=0b100100100;
+			return newTune;
 		}
 		else if (AlarmsGet(SYSTEMALARMS_ALARM_GPS) > SYSTEMALARMS_ALARM_WARNING) {
-			*newTune=0b100100100;
-			return;
+			uint32_t newTune=0b100100100;
+			return newTune ;
 		}
 	}
 	
 	//no tune found
-	*newTune=0;
-	
-	return;
+	return 0;
 }
 
 /**
