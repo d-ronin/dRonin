aa7b9628738c284e053fea46cbdea8a4768bd732
Martin Luessi <mluessi@gmail.com>
2016-05-03 14:48:02 -0400

    BrainFPV RE1 fw files

---
+++ flight/targets/brainre1/board-info/board-info.mk
@@ -0,0 +1,38 @@
+BOARD_TYPE          := 0x8B
+BOARD_REVISION      := 0x01
+BOOTLOADER_VERSION  := 0x83
+HW_TYPE             := 0x00		# seems to be unused
+
+MCU                 := cortex-m4
+CHIP                := STM32F446RET
+BOARD               := STM32F4xx_BRAINFPVRE1
+STM32_TYPE          := STM32F446xx
+MODEL               := HD
+MODEL_SUFFIX        := 
+
+OPENOCD_JTAG_CONFIG := interface/stlink-v2-1.cfg
+OPENOCD_CONFIG      := stm32f4x.cfg
+
+# Note: These must match the values in link_$(BOARD)_memory.ld
+BL_BANK_BASE        := 0x08000000  # Start of bootloader flash
+BL_BANK_SIZE        := 0x00008000  # Should include BD_INFO region (32kb)
+
+# Leave the remaining 16KB and 64KB sectors for other uses
+FW_BANK_BASE        := 0x08020000  # Start of firmware flash (128kb)
+FW_BANK_SIZE        := 0x00060000  # Should include FW_DESC_SIZE (256kb)
+
+FW_DESC_SIZE        := 0x00000064
+
+EE_BANK_BASE        := 0x00000000
+EE_BANK_SIZE        := 0x00000000
+
+EF_BANK_BASE        := 0x08000000  # Start of entire flash image (usually start of bootloader as well)
+EF_BANK_SIZE        := 0x00080000  # Size of the entire flash image (from bootloader until end of firmware)
+
+OSCILLATOR_FREQ     :=  16000000
+SYSCLK_FREQ         := 180000000
+
+# Include these here, so they get included in the firmware and bootloader
+SRC += $(BOARD_INFO_DIR)/../re1fpga/fpga_drv.c
+EXTRAINCDIRS += $(BOARD_INFO_DIR)/../re1fpga/
+
