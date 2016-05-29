7cbf8e95ca3de6929cde3e14456355fd0e212d9e
DTF UHF <dtfuhf@gmail.com>
2016-05-29 09:49:47 -0400

    Adjust telemetry serial handle pointer name to match change in fastforward

--- flight/targets/DTFc/board-info/pios_board.h
+++ flight/targets/DTFc/board-info/pios_board.h
@@ -108,9 +108,9 @@ TIM17 |           |xxxxxxxxxxx|xxxxxxxxxxx|xxxxxxxxxx
 //
 // See also pios_board.c
 //-------------------------
-extern uintptr_t pios_com_telem_rf_id;
 extern uintptr_t pios_com_gps_id;
 extern uintptr_t pios_com_telem_usb_id;
+extern uintptr_t pios_com_telem_serial_id;
 extern uintptr_t pios_com_bridge_id;
 extern uintptr_t pios_com_vcp_id;
 extern uintptr_t pios_com_mavlink_id;
@@ -124,7 +124,7 @@ extern uintptr_t pios_com_storm32bgc_id;
 
 #define PIOS_COM_GPS                    (pios_com_gps_id)
 #define PIOS_COM_TELEM_USB              (pios_com_telem_usb_id)
-#define PIOS_COM_TELEM_RF               (pios_com_telem_rf_id)
+#define PIOS_COM_TELEM_RF               (pios_com_telem_serial_id)
 #define PIOS_COM_BRIDGE                 (pios_com_bridge_id)
 #define PIOS_COM_VCP                    (pios_com_vcp_id)
 #define PIOS_COM_MAVLINK                (pios_com_mavlink_id)
