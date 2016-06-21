38a7654aa6423d12393aa386a5801ff0a6dcd0e4
Michael Corcoran <tracer@outlook.co.nz>
2016-06-21 17:10:58 +1200

    DTFc: Rename files to lowercase (matches all other targets)

--- flight/targets/dtfc/target-defs.mk
+++ flight/targets/dtfc/target-defs.mk
@@ -1,10 +1,10 @@
 # Add this board to the list of buildable boards
-ALL_BOARDS += DTFc
+ALL_BOARDS += dtfc
 
 # Set the cpu architecture here that matches your STM32
 # Should be one of: f1,f3,f4
-DTFc_cpuarch := f3
+dtfc_cpuarch := f3
 
 # Short name of this board (used to display board name in parallel builds)
 # Should be exactly 4 characters long.
-DTFc_short := 'DTFc'
+dtfc_short := 'dtfc'
