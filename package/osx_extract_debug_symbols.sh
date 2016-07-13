4d6056dca965d8b7516ec1a1619183be58e64d97
Michael Corcoran <mrc93@uclive.ac.nz>
2016-07-13 18:36:14 +1200

    Package: Dump all symbol info on OSX

--- package/osx_extract_debug_symbols.sh
+++ package/osx_extract_debug_symbols.sh
@@ -25,12 +25,14 @@ function generateSymbols()
 {
   filename=$(basename "$1")
   debugfile="$debugdir/$filename.sym"
+  dsymfile="$debugdir/$filename.dSYM"
   if [ ! -d "${debugdir}" ] ; then
     echo "creating dir ${debugdir}"
     mkdir -p "${debugdir}"
   fi
   echo "Generating symbols for $1, putting them in ${debugfile}"
-  ${DUMP_SYMBOLS_TOOL} ${SOURCE_DIR}/${1} > ${debugfile}
+  dsymutil -o ${dsymfile} ${SOURCE_DIR}/${1}
+  ${DUMP_SYMBOLS_TOOL} -g ${dsymfile} ${SOURCE_DIR}/${1} > ${debugfile}
   echo "Striping debug information from $1"
   strip -S "${SOURCE_DIR}/${1}"
 }
