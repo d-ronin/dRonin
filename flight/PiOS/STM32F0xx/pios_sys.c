6f1b41f8016344830197b20055f35e19c4b7565b
Michael Lyle <mlyle@lyle.org>
2016-07-26 20:42:45 -0700

    pios: further work on stm32f0xx

--- flight/PiOS/STM32F0xx/pios_sys.c
+++ flight/PiOS/STM32F0xx/pios_sys.c
@@ -10,6 +10,7 @@
  * @author     Michael Smith Copyright (C) 2011
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2012.
  * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2014
+ * @author     dRonin, http://dronin.org, Copyright (C) 2016
  * @brief      Sets up basic STM32 system hardware, functions are called from Main.
  * @see        The GNU Public License (GPL) Version 3
  *
@@ -45,11 +46,9 @@ static void NVIC_Configuration(void);
 */
 void PIOS_SYS_Init(void)
 {
-#if !defined(PIOS_INCLUDE_CHIBIOS)
 	/* Setup STM32 system (RCC, clock, PLL and Flash configuration) - CMSIS Function */
 	SystemInit();
 	SystemCoreClockUpdate();	/* update SystemCoreClock for use elsewhere */
-#endif /* !defined(PIOS_INCLUDE_CHIBIOS) */
 
 	/*
 	 * @todo might make sense to fetch the bus clocks and save them somewhere to avoid
@@ -60,6 +59,7 @@ void PIOS_SYS_Init(void)
 	/* do this early to ensure that we take exceptions in the right place */
 	NVIC_Configuration();
 
+#if 0
 	/* Init the delay system */
 	PIOS_DELAY_Init();
 
@@ -117,28 +117,7 @@ void PIOS_SYS_Init(void)
 			       RCC_APB2Periph_SYSCFG |
 			0, ENABLE);
 
-	/*
-	 * Configure all pins as input / pullup to avoid issues with
-	 * uncommitted pins, excepting special-function pins that we need to
-	 * remain as-is.
-	 */
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_StructInit(&GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;	// default is un-pulled input
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
-#if (PIOS_USB_ENABLED)
-	GPIO_InitStructure.GPIO_Pin &= ~(GPIO_Pin_11 | GPIO_Pin_12);				// leave USB D+/D- alone
 #endif
-	GPIO_InitStructure.GPIO_Pin &= ~(GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);	// leave JTAG pins alone
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-	GPIO_Init(GPIOE, &GPIO_InitStructure);
-	GPIO_Init(GPIOF, &GPIO_InitStructure);
 }
 
 /**
@@ -222,17 +201,10 @@ int32_t PIOS_SYS_SerialNumberGet(char *str)
 }
 
 /**
-* Configures Vector Table base location and SysTick
+* Configures SysTick
 */
 static void NVIC_Configuration(void)
 {
-	/* Set the Vector Table base address as specified in .ld file */
-	extern void *pios_isr_vector_table_base;
-	NVIC_SetVectorTable((uint32_t)&pios_isr_vector_table_base, 0x0);
-
-	/* 4 bits for Interrupt priorities so no sub priorities */
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
-
 	/* Configure HCLK clock as SysTick clock source. */
 	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK);
 }
