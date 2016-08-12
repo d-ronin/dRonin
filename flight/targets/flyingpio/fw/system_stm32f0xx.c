c797ae7cad421a23af6d151b93ea204dc07a75c0
Michael Lyle <mlyle@lyle.org>
2016-08-11 23:11:05 -0700

    flyingpio: correct clock freq to desired 40MHz

--- flight/targets/flyingpio/fw/system_stm32f0xx.c
+++ flight/targets/flyingpio/fw/system_stm32f0xx.c
@@ -296,7 +296,7 @@ static void SetSysClock(void)
 
     /* PLL configuration */
     RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMULL));
-    RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_PREDIV1 | RCC_CFGR_PLLXTPRE_PREDIV1 | RCC_CFGR_PLLMULL6);
+    RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_PREDIV1 | RCC_CFGR_PLLXTPRE_PREDIV1 | RCC_CFGR_PLLMULL5);
             
     /* Enable PLL */
     RCC->CR |= RCC_CR_PLLON;
