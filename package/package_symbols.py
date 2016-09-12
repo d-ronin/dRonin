fdf54337e96c14ce640a24a4a098f0b6decab8a5
Michael Corcoran <tracer@outlook.co.nz>
2016-09-12 21:09:50 +1200

    Package: Create file with binary path -> breakpad UUID map
    Will make setting up to debug a minidump in gdb easier

--- package/package_symbols.py
+++ package/package_symbols.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
+import sys
 import os
 import platform
 import subprocess
@@ -45,9 +48,14 @@ class GenericSymbolDump(object):
 		self.breakpad_path = os.path.realpath("./")
 		self.debug_path = os.path.realpath("./")
 		self.dump_syms = "dump_syms" # let's hope it's on path if this stays
+		self.output_map = False
+		self.map_file = None
 
 	def set_dump_syms_path(self, path):
 		self.dump_syms = os.path.realpath(path)
+
+	def generate_map(self, enable):
+		self.output_map = bool(enable)
 
 	def enumerate_targets(self):
 		for file in recursive_file_gen(self.root_path):
@@ -80,13 +88,22 @@ class GenericSymbolDump(object):
 
 	def generate_breakpad_symbols(self):
 		info("Using dump_syms: " + self.dump_syms)
+		if self.output_map:
+			mkdir_if_not_exist(self.breakpad_path)
+			with open(os.path.join(self.breakpad_path, 'breakpad.map'), 'w') as fp:
+				print('binary|uuid', file=fp)
+				self._generate_breakpad_symbols(fp)
+		else:
+			self._generate_breakpad_symbols()
+
+	def _generate_breakpad_symbols(self, mapfile = None):
 		for t in self.targets:
 			info("Processing breakpad symbols for: " + os.path.basename(self.targets[t]["source_file"]))
 			cmd = self.get_dump_syms_cmd(self.targets[t]["source_file"], self.targets[t]["debug_symbols"])
 			if cmd:
-				self.process_dump_syms(cmd)
+				self.process_dump_syms(cmd, self.targets[t]["source_file"], mapfile)
 
-	def process_dump_syms(self, cmd):
+	def process_dump_syms(self, cmd, objpath, mapfile = None):
 		syms = ""
 		try:
 			proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -97,7 +114,7 @@ class GenericSymbolDump(object):
 		syms, err = proc.communicate()
 
 		if proc.returncode != 0:
-			warning("Invalid symbols [{0}]: {1}".format(proc.returncode, cmd[-1]))
+			warning("Invalid symbols [{0}]: {1}".format(proc.returncode, objpath))
 			warning("Command: " + " ".join(cmd))
 			warning(err)
 			return
@@ -111,11 +128,14 @@ class GenericSymbolDump(object):
 			warning("Invalid symbol dump: {1}".format(cmd[-1]))
 			return
 		osys, arch, uuid, name = module[1:]
-		
+
 		out_path = os.path.join(self.breakpad_path, name, uuid, self.get_symbol_name(name))
 		mkdir_if_not_exist(os.path.dirname(out_path))
 		with open(out_path, "wb") as fp:
 			fp.write(syms)
+
+		if mapfile:
+			self._add_map_entry(mapfile, os.path.relpath(objpath, self.root_path), uuid)
 
 	def move_debug_syms(self):
 		for t in self.targets:
@@ -125,6 +145,9 @@ class GenericSymbolDump(object):
 				info("Moving {0}".format(rel))
 				mkdir_if_not_exist(os.path.dirname(new_path))
 				shutil.move(self.targets[t]["debug_symbols"], new_path)
+
+	def _add_map_entry(self, fp, bin_name, uuid):
+		print('{}|{}'.format(bin_name, uuid), file=fp)
 
 class MacOSSymbolDump(GenericSymbolDump):
 	def __init__(self, path):
@@ -218,6 +241,7 @@ class LinuxSymbolDump(GenericSymbolDump):
 
 def main():
 	parser = argparse.ArgumentParser()
+	parser.add_argument("-m", "--map", action='count', help="Generate mapfile with object -> UUID mapping")
 	parser.add_argument("-p", "--pkgpath", default="package", nargs="?", help="Package directory, -symbols will be appended")
 	parser.add_argument("-d", "--dumpsyms", nargs="?", help="dump_syms binary to use when generating breakpad symbols")
 	parser.add_argument("-l", "--logfile", nargs="?", help="Logfile path to output debug information")
@@ -258,6 +282,7 @@ def main():
 
 	if args.dumpsyms:
 		dumper.set_dump_syms_path(args.dumpsyms)
+	dumper.generate_map(args.map > 0)
 	dumper.generate_breakpad_symbols()
 	# this is done earlier in build process to work around macdeployqt bug
 	if platform.system() != "Darwin":
@@ -266,4 +291,4 @@ def main():
 	return 0
 
 if __name__ == "__main__":
-	exit(main())
+	sys.exit(main())
