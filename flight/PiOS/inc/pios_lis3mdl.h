6a0ee1d0ed067d88d69dd39029cae6111e9762a9
Michael Lyle <mlyle@lyle.org>
2016-10-30 13:03:26 -0700

    pios: preliminary lis3mdl driver

--- flight/PiOS/inc/pios_lis3mdl.h
+++ flight/PiOS/inc/pios_lis3mdl.h
@@ -1,14 +1,12 @@
 /**
  ******************************************************************************
- * @file       pios_bmm150.h
- * @author     dRonin, http://dRonin.org/, Copyright (C) 2015
- * @author     Tau Labs, http://taulabs.org, Copyright (C) 2012-2015
- * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2012.
+ * @file       pios_lis3mdl.h
+ * @author     dRonin, http://dRonin.org/, Copyright (C) 2016
  * @addtogroup PIOS PIOS Core hardware abstraction layer
  * @{
- * @addtogroup PIOS_BMX055
+ * @addtogroup PIOS_LIS3MDL
  * @{
- * @brief Driver for Bosch BMM150 IMU Sensor
+ * @brief Driver for ST LIS3MDL Magnetometer
  *****************************************************************************/
 /*
  * This program is free software; you can redistribute it and/or modify
@@ -24,37 +22,41 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Additional note on redistribution: The copyright and license notices above
+ * must be maintained in each individual source file that is a derivative work
+ * of this source file; otherwise redistribution is prohibited.
  */
 
-#ifndef PIOS_BMM150_H
-#define PIOS_BMM150_H
+#ifndef PIOS_LIS3MDL_H
+#define PIOS_LIS3MDL_H
 
 #include "pios.h"
 
-enum pios_bmm150_orientation { // clockwise rotation from board forward
-	PIOS_BMM_TOP_0DEG    = 0x00,
-	PIOS_BMM_TOP_90DEG   = 0x01,
-	PIOS_BMM_TOP_180DEG  = 0x02,
-	PIOS_BMM_TOP_270DEG  = 0x03,
-	PIOS_BMM_BOTTOM_0DEG  = 0x04,
-	PIOS_BMM_BOTTOM_90DEG  = 0x05,
-	PIOS_BMM_BOTTOM_180DEG  = 0x06,
-	PIOS_BMM_BOTTOM_270DEG  = 0x07,
+enum pios_lis3mdl_orientation { // clockwise rotation from board forward
+	PIOS_LIS_TOP_0DEG    = 0x00,
+	PIOS_LIS_TOP_90DEG   = 0x01,
+	PIOS_LIS_TOP_180DEG  = 0x02,
+	PIOS_LIS_TOP_270DEG  = 0x03,
+	PIOS_LIS_BOTTOM_0DEG  = 0x04,
+	PIOS_LIS_BOTTOM_90DEG  = 0x05,
+	PIOS_LIS_BOTTOM_180DEG  = 0x06,
+	PIOS_LIS_BOTTOM_270DEG  = 0x07,
 };
 
-struct pios_bmm150_cfg {
-	enum pios_bmm150_orientation orientation;
+struct pios_lis3mdl_cfg {
+	enum pios_lis3mdl_orientation orientation;
 };
 
-typedef struct pios_bmm150_dev * pios_bmm150_dev_t;
+typedef struct lis3mdl_dev * lis3mdl_dev_t;
 
 /**
- * @brief Initialize the BMM-xxxx 6/9-axis sensor on SPI
+ * @brief Initialize the LIS3MDL mag
  * @return 0 for success, -1 for failure to allocate, -10 for failure to get irq
  */
-int32_t PIOS_BMM150_SPI_Init(pios_bmm150_dev_t *dev, uint32_t spi_id, uint32_t slave_mag, const struct pios_bmm150_cfg *cfg);
+int32_t PIOS_LIS3MDL_SPI_Init(lis3mdl_dev_t *dev, uint32_t spi_id, uint32_t slave_mag, const struct pios_lis3mdl_cfg *cfg);
 
-#endif /* PIOS_BMM150_H */
+#endif /* PIOS_LIS3MDL_H */
 
 /** 
   * @}
