2a1646621064cab5ebb3195ad72454226bff11f6
Michael Corcoran <tracer@outlook.co.nz>
2017-02-14 17:46:30 +1300

    Add USB ID code generator and scripts
    Runs under Py 2 or 3

---
+++ shared/usb_ids/header_template.h
@@ -0,0 +1,53 @@
+/**
+ ******************************************************************************
+ * @file       ${FILENAME}
+ * @author     dRonin
+ * @addtogroup Shared Shared Definitions
+ * @{
+ * @addtogroup USB_ID USB Identifiers
+ * @{
+ * @brief USB VID/PIDs used in dRonin
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * Additional note on redistribution: The copyright and license notices above
+ * must be maintained in each individual source file that is a derivative work
+ * of this source file; otherwise redistribution is prohibited.
+ */
+
+/*
+ * NOTE: Automatically generated, DO NOT MODIFY (see shared/usb_ids)
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum dronin_usb_vids {
+${VIDS}
+};
+
+enum dronin_usb_pids {
+${PIDS}
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ * @}
+ */
