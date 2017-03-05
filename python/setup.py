a289e428b279c099794c61ac4ecde175ae92a810
Michael Lyle <mlyle@lyle.org>
2017-03-05 08:28:26 -0800

    python-api: bump py version for pypi/pip
    Going to push a release to pypi at minimum today.

--- python/setup.py
+++ python/setup.py
@@ -26,7 +26,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='20160825',
+    version='20170305',
 
     description='dRonin Python API',
     long_description=long_description,
