ad402c17f3005a41dcb1357b841b173f38bfffd6
Michael Corcoran <tracer@outlook.co.nz>
2017-06-23 18:14:05 +1200

    GCS:UAVSettingsImportExport: Disable commands when not connected

--- ground/gcs/src/plugins/uavsettingsimportexport/uavsettingsimportexportmanager.cpp
+++ ground/gcs/src/plugins/uavsettingsimportexport/uavsettingsimportexportmanager.cpp
@@ -41,6 +41,7 @@
 #include <coreplugin/coreconstants.h>
 #include <coreplugin/actionmanager/actionmanager.h>
 #include <coreplugin/icore.h>
+#include <uavtalk/telemetrymanager.h>
 #include <QKeySequence>
 
 // for UAVObjects
@@ -65,9 +66,10 @@ UAVSettingsImportExportManager::~UAVSettingsImportExportManager()
 UAVSettingsImportExportManager::UAVSettingsImportExportManager(QObject *parent)
     : QObject(parent)
 {
+    auto core = Core::ICore::instance();
 
     // Add Menu entry
-    Core::ActionManager *am = Core::ICore::instance()->actionManager();
+    Core::ActionManager *am = core->actionManager();
     Core::ActionContainer *ac = am->actionContainer(Core::Constants::M_FILE);
     Core::Command *cmd =
         am->registerAction(new QAction(this), "UAVSettingsImportExportPlugin.UAVSettingsExport",
@@ -90,6 +92,16 @@ UAVSettingsImportExportManager::UAVSettingsImportExportManager(QObject *parent)
     cmd->action()->setText(tr("Export UAV Data..."));
     ac->addAction(cmd, Core::Constants::G_HELP_HELP);
     connect(cmd->action(), SIGNAL(triggered(bool)), this, SLOT(exportUAVData()));
+}
+
+void UAVSettingsImportExportManager::extensionsInitialized()
+{
+    auto pm = ExtensionSystem::PluginManager::instance();
+    auto telemetry = pm->getObject<TelemetryManager>();
+    // enabled/disabled commands when board is connected/disconnected
+    connect(telemetry, &TelemetryManager::connectedChanged,
+            this, &UAVSettingsImportExportManager::setCommandsEnabled);
+    setCommandsEnabled(telemetry->isConnected());
 }
 
 bool UAVSettingsImportExportManager::importUAVSettings(const QByteArray &settings, bool quiet)
@@ -523,4 +535,21 @@ void UAVSettingsImportExportManager::exportUAVData()
     msgBox.setText(tr("Data saved."));
     msgBox.setStandardButtons(QMessageBox::Ok);
     msgBox.exec();
+}
+
+void UAVSettingsImportExportManager::setCommandsEnabled(bool enabled)
+{
+    static const std::vector<QString> commands {
+        QStringLiteral("UAVSettingsImportExportPlugin.UAVSettingsExport"),
+        QStringLiteral("UAVSettingsImportExportPlugin.UAVSettingsImport"),
+        QStringLiteral("UAVSettingsImportExportPlugin.UAVDataExport")
+    };
+
+    Core::ActionManager *am = Core::ICore::instance()->actionManager();
+    if (!am)
+        return;
+
+    for (const auto &command : commands)
+        if (Core::Command *cmd = am->command(command))
+            cmd->action()->setEnabled(enabled);
 }
