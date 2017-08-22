75fb8b1392a65ea806bd57b0a47b66ccf0277655
Michael Lyle <mlyle@lyle.org>
2017-08-22 11:24:43 -0700

    flight: loadable: iterate over extension chunks

--- flight/Modules/Loadable/loadable.c
+++ flight/Modules/Loadable/loadable.c
@@ -30,6 +30,8 @@
 #include <stdbool.h>
 #include "pios_thread.h"
 
+#include "loadable_extension.h"
+
 #include "modulesettings.h"
 
 static bool module_enabled;
@@ -53,14 +55,76 @@ static int32_t loadable_initialize(void)
 
 static void loadable_task(void *parameters);
 
+static void invoke_one_loadable(struct loadable_extension *ext)
+{
+	/* XXX check payload CRC. */
+}
+
+static void invoke_loadables()
+{
+	uintptr_t part_id;
+
+	if (PIOS_FLASH_find_partition_id(FLASH_PARTITION_LABEL_LOADABLE_EXTENSION,
+				&part_id)) {
+		/* No loadable partition */
+		return;
+	}
+
+	uint32_t part_len;
+	uint8_t *part_beginning;
+
+	part_beginning = PIOS_FLASH_get_address(part_id, &part_len);
+
+	if (!part_beginning) {
+		return;
+	}
+
+	uint32_t part_pos = 0;
+
+	while ((part_pos + sizeof(struct loadable_extension)) < part_len) {
+		struct loadable_extension *ext =
+			(void *) (part_beginning + part_pos);
+
+		/* Validate header before trying to go further */
+
+		if (ext->magic != LOADABLE_EXTENSION_MAGIC) {
+			return;
+		}
+
+		if (ext->length < sizeof(struct loadable_extension)) {
+			return;
+		}
+
+		if (ext->require_version == LOADABLE_REQUIRE_VERSION_INVALID) {
+			return;
+		}
+
+		if (ext->entry_offset >= ext->length) {
+			return;
+		}
+
+		if (ext->entry_offset < sizeof(struct loadable_extension)) {
+			return;
+		}
+
+		/* XXX alignment requirements */
+
+		/* XXX check CRC */
+
+		invoke_one_loadable(ext);
+
+		part_pos += ext->length;
+	}
+
+}
+
 static int32_t loadable_start(void)
 {
 	if (!module_enabled) {
 		return 0;
 	}
 
-	/* XXX: check for loadable task, requested stack size, etc */
-
+	/* XXX 	for now do stuff in a task, in an infinite loop */
 	struct pios_thread *loadable_task_handle;
 
 	loadable_task_handle = PIOS_Thread_Create(loadable_task, "Fault", PIOS_THREAD_STACK_SIZE_MIN, NULL, PIOS_THREAD_PRIO_HIGHEST);
@@ -73,13 +137,15 @@ static int32_t loadable_start(void)
 
 	return 0;
 }
+
+
 MODULE_INITCALL(loadable_initialize, loadable_start)
 
 static void loadable_task(void *parameters)
 {
-	/* XXX: invoke loadable task */
 	while (1) {
 		PIOS_Thread_Sleep(1000);
+		invoke_loadables();
 	}
 }
 
