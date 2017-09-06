2fda21d81e1e1fa607396c0dae5626f3d019caf5
Michael Corcoran <tracer@outlook.co.nz>
2017-09-06 21:53:21 +1200

    GCS: Fix up use of deprecated functions, enable warnings

--- ground/gcs/src/libs/qwt/src/qwt_thermo.cpp
+++ ground/gcs/src/libs/qwt/src/qwt_thermo.cpp
@@ -19,6 +19,8 @@
 #include <qstyleoption.h>
 #include <qmath.h>
 
+#include <functional>
+
 static inline void qwtDrawLine( QPainter *painter, int pos, 
     const QColor &color, const QRect &pipeRect, const QRect &liquidRect,
     Qt::Orientation orientation )
@@ -560,9 +562,9 @@ void QwtThermo::drawLiquid(
         QVector<double> values = qwtTickList( scaleDraw()->scaleDiv() );
 
         if ( scaleMap.isInverting() )
-            qSort( values.begin(), values.end(), qGreater<double>() );
+            std::sort( values.begin(), values.end(), std::greater<double>() );
         else
-            qSort( values.begin(), values.end(), qLess<double>() );
+            std::sort( values.begin(), values.end(), std::less<double>() );
 
         int from;
         if ( !values.isEmpty() )
