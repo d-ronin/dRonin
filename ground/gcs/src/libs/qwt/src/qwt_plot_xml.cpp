87414cd564806297deff85bf4b29f6226b67c34d
Michael Corcoran <tracer@outlook.co.nz>
2017-10-07 01:54:19 +1300

    Qwt: Fix usage of QString::null, deprecated in Qt 5.9

--- ground/gcs/src/libs/qwt/src/qwt_plot_xml.cpp
+++ ground/gcs/src/libs/qwt/src/qwt_plot_xml.cpp
@@ -37,6 +37,6 @@ QString QwtPlot::grabProperties() const
     // Temporary dummy code, for designer tests
     return title().text();
 #else
-    return QString::null;
+    return QString();
 #endif
 }
