aeed1892fc1c0f614f9d371a52ba72871d3b9899
Michael Corcoran <tracer@outlook.co.nz>
2017-10-20 14:16:10 +1300

    PiOS: Mark PIOS_DEBUG_Panic noreturn
    Prevents build failures where gcc sees unitialised variables in code paths where PIOS_DEBUG_Assert (expands to PIOS_DEBUG_Panic call) returns

--- flight/PiOS/inc/pios_debug.h
+++ flight/PiOS/inc/pios_debug.h
@@ -6,24 +6,24 @@
  * @brief Debugging functionality
  * @{
  *
- * @file       pios_i2c.h  
+ * @file       pios_i2c.h
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
  * @brief      Debug helper functions header.
  * @see        The GNU Public License (GPL) Version 3
  *
  *****************************************************************************/
-/* 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 3 of the License, or 
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
- * 
- * You should have received a copy of the GNU General Public License along 
+ *
+ * You should have received a copy of the GNU General Public License along
  * with this program; if not, see <http://www.gnu.org/licenses/>
  */
 
@@ -42,7 +42,7 @@ void PIOS_DEBUG_PinHigh(uint8_t pin);
 void PIOS_DEBUG_PinLow(uint8_t pin);
 void PIOS_DEBUG_PinValue8Bit(uint8_t value);
 void PIOS_DEBUG_PinValue4BitL(uint8_t value);
-void PIOS_DEBUG_Panic(const char *msg);
+void PIOS_DEBUG_Panic(const char *msg) __attribute__((noreturn));
 
 #ifdef DEBUG
 #define PIOS_DEBUG_Assert(test) if (!(test)) PIOS_DEBUG_Panic(PIOS_DEBUG_AssertMsg);
