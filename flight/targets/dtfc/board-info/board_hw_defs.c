5c9d85012c902fa7a9aeeacd4733246311857e67
jihlein <jihlein@hotmail.com>
2018-08-24 21:40:56 -0600

    Move DTFc from 1000 Hz to 500 Hz frame rate

--- flight/targets/dtfc/board-info/board_hw_defs.c
+++ flight/targets/dtfc/board-info/board_hw_defs.c
@@ -1030,7 +1030,7 @@ static const struct pios_exti_cfg pios_exti_mpu_cfg __exti_config = {
 
 static struct pios_mpu_cfg pios_mpu_cfg = {
     .exti_cfg = &pios_exti_mpu_cfg,
-    .default_samplerate = 1000,
+    .default_samplerate = 500,
     .orientation = PIOS_MPU_TOP_180DEG
 };
 #endif /* PIOS_INCLUDE_MPU */
